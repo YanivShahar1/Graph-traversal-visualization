676ddb66ad6e4a3cbf329358f056520ef4d3d671
Yaniv Shahar <103381319+YanivShahar1@users.noreply.github.com>
2022-05-21 19:00:47 +0300

    Update graph_traversal.py

--- graph_traversal.py
+++ graph_traversal.py
@@ -6,9 +6,14 @@ found = False
 def search(path, walls, start, end, square, color="green", bfs=False, dfs=False):
     global found
 
-    queue = []
     visited = set()
-    frontier = deque()
+    
+    #bfs - deque(queue), dfs - list(stack)
+    if bfs:
+        frontier = deque()
+
+    elif dfs:
+        frontier = []
 
     square.color(color)
     frontier.append(start)
@@ -18,30 +23,31 @@ def search(path, walls, start, end, square, color="green", bfs=False, dfs=False)
             x, y = frontier.popleft()
 
         elif dfs:
-            x, y = frontier.pop()
+            x, y = frontier.pop(-1)
 
-        if (x - 24, y) in path and (x - 24, y) not in visited:  
+        if (x - 24, y) in path and (x - 24, y) not in visited:  # check the cell on the left
             cell = (x - 24, y)
             if bfs:
                 solution[cell] = x, y
-            frontier.append(cell) 
-            visited.add((x - 24, y))  
 
-        if (x, y - 24) in path and (x, y - 24) not in visited:  
+            frontier.append(cell)  # add cell to frontier list
+            visited.add((x - 24, y))  # add cell to visited list
+
+        if (x, y - 24) in path and (x, y - 24) not in visited:  # check the cell down
             cell = (x, y - 24)
             if bfs:
                 solution[cell] = x, y
             frontier.append(cell)
             visited.add((x, y - 24))
 
-        if (x + 24, y) in path and (x + 24, y) not in visited: 
+        if (x + 24, y) in path and (x + 24, y) not in visited:  # check the cell on the  right
             cell = (x + 24, y)
             if bfs:
                 solution[cell] = x, y
             frontier.append(cell)
             visited.add((x + 24, y))
 
-        if (x, y + 24) in path and (x, y + 24) not in visited:  
+        if (x, y + 24) in path and (x, y + 24) not in visited:  # check the cell up
             cell = (x, y + 24)
             if bfs:
                 solution[cell] = x, y
@@ -63,11 +69,11 @@ def backRoute(s_x, s_y, e_x,e_y,square,color = "yellow"):
         print('found!')
         x, y = e_x, e_y
         square.color(color)
-        square.goto(solution[x, y])  
+        square.goto(solution[x, y])  # move the yellow sprite to the key value of solution ()
         square.stamp()
-        while not (x, y) == (s_x, s_y):   
-            x, y = solution[x, y]               
-            square.goto(solution[x, y])  
+        while not (x, y) == (s_x, s_y):    # stop loop when current cells == start cell
+            x, y = solution[x, y]               # "key value" now becomes the new key
+            square.goto(solution[x, y])  # move the yellow sprite to the key value of solution ()
             square.stamp()
 
     return True if found else False
